373b2abb266b9851a5dd5b9508c7df445e51eed4
Leon Venticinque <Leonbusiness@icloud.com>
2026-08-08 15:12:10 +0200

    Add dynamic loading for navbar and footer

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Navigation laden
+    fetch("/components/navbar.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Navigation konnte nicht geladen werden.");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+
+    // Footer laden
+    fetch("/components/footer.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Footer konnte nicht geladen werden.");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+});
