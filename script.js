dfddbdc75b78503a9e0cb289608b417ddea50347
Leon Venticinque <Leonbusiness@icloud.com>
2026-08-08 15:21:59 +0200

    Remove leading slash from fetch URLs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // Navigation laden
-    fetch("/components/navbar.html")
+    fetch("components/navbar.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Navigation konnte nicht geladen werden.");
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // Footer laden
-    fetch("/components/footer.html")
+    fetch("components/footer.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Footer konnte nicht geladen werden.");
