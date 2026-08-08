05ca4bfd2db97fd1dd30876ee2bcccf133393ced
Leon Venticinque <Leonbusiness@icloud.com>
2026-08-08 15:48:03 +0200

    Update error messages for navbar and footer fetch

--- script.js
+++ script.js
@@ -3,32 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("./components/navbar.html")
         .then(response => {
             if (!response.ok) {
-                throw new Error("Navbar konnte nicht geladen werden: " + response.status);
+                throw new Error("Navigation konnte nicht geladen werden.");
             }
-
             return response.text();
         })
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
         })
         .catch(error => {
-            console.error(error);
+            console.error("Navbar-Fehler:", error);
         });
 
 
     fetch("./components/footer.html")
         .then(response => {
             if (!response.ok) {
-                throw new Error("Footer konnte nicht geladen werden: " + response.status);
+                throw new Error("Footer konnte nicht geladen werden.");
             }
-
             return response.text();
         })
         .then(data => {
             document.getElementById("footer").innerHTML = data;
         })
         .catch(error => {
-            console.error(error);
+            console.error("Footer-Fehler:", error);
         });
 
 });
