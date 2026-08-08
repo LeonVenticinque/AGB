4196ca88e32030119fe2268fa3dcbf64624521c5
Leon Venticinque <Leonbusiness@icloud.com>
2026-08-08 15:41:18 +0200

    Fix fetch paths and error messages in script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Navigation laden
-    fetch("components/navbar.html")
+    fetch("./components/navbar.html")
         .then(response => {
             if (!response.ok) {
-                throw new Error("Navigation konnte nicht geladen werden.");
+                throw new Error("Navbar konnte nicht geladen werden: " + response.status);
             }
+
             return response.text();
         })
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
         })
         .catch(error => {
-            console.error("Navbar-Fehler:", error);
+            console.error(error);
         });
 
 
-    // Footer laden
-    fetch("components/footer.html")
+    fetch("./components/footer.html")
         .then(response => {
             if (!response.ok) {
-                throw new Error("Footer konnte nicht geladen werden.");
+                throw new Error("Footer konnte nicht geladen werden: " + response.status);
             }
+
             return response.text();
         })
         .then(data => {
             document.getElementById("footer").innerHTML = data;
         })
         .catch(error => {
-            console.error("Footer-Fehler:", error);
+            console.error(error);
         });
 
 });
